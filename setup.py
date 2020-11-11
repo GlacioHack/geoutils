fd31be11bca676bcbe700d9e05bc898a7e8da019
Bob McNabb <robertmcnabb@gmail.com>
2020-11-11 00:54:17 +0000

    initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='GeoUtils',
+      version='0.1',
+      description='',
+      url='',
+      author='The GlacioHack Team',
+      license='BSD-3',
+      packages=['GeoUtils'],
+      install_requires=['rasterio'],
+      scripts=[],
+      zip_safe=False)
