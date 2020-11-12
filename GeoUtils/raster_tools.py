81af3cd7b92b90f230df75bd41e9856b5b4c1d3d
Andrew Tedstone <andrew.tedstone@unifr.ch>
2020-11-12 11:11:36 +0100

    add dummy __repr__ func (needs modification); add __str__ func; modify info() to playnicelywith __str__

--- GeoUtils/raster_tools.py
+++ GeoUtils/raster_tools.py
@@ -16,24 +16,40 @@ class Raster():
         self.ds = ds
         
         # Copy most used attributes/methods
+        self._saved_attrs = saved_attrs
         for attr in saved_attrs:
             setattr(self, attr, getattr(ds,attr)) 
+
+
+    def __repr__(self):
+    	""" Convert object to formal string representation. """
+    	L = [getattr(self, item) for item in self._saved_attrs]
+    	s = "%s.%s(%s)" % (self.__class__.__module__,
+    		self.__class__.__qualname__,
+    		", ".join(map(str, L)))
+
+    	return s
+
+
+    def __str__(self):
+    	return self.info()
+
         
     def info(self):
         """ 
-        Prints information about the raster (filename, coordinate system, number of columns/rows, etc.).
+        Returns string of information about the raster (filename, coordinate system, number of columns/rows, etc.).
         """
-        print('Driver:             {}'.format(self.driver))
-        #        if self.intype != 'MEM':
-        print('File:               {}'.format(self.name))
-        #else:
-        #    print('File:               {}'.format('in memory'))
-        print('Size:               {}, {}'.format(self.width, self.height))
-        print('Coordinate System:  EPSG:{}'.format(self.crs.to_epsg()))
-        print('NoData Value:       {}'.format(self.nodata))
-        print('Pixel Size:         {}, {}'.format(*self.res))
-        print('Upper Left Corner:  {}, {}'.format(*self.bounds[:2]))
-        print('Lower Right Corner: {}, {}'.format(*self.bounds[2:]))
+        as_str = []
+        as_str.append('Driver:             {} \n'.format(self.driver))
+        as_str.append('File:               {}\n'.format(self.name))
+        as_str.append('Size:               {}, {}\n'.format(self.width, self.height))
+        as_str.append('Coordinate System:  EPSG:{}\n'.format(self.crs.to_epsg()))
+        as_str.append('NoData Value:       {}\n'.format(self.nodata))
+        as_str.append('Pixel Size:         {}, {}\n'.format(*self.res))
+        as_str.append('Upper Left Corner:  {}, {}\n'.format(*self.bounds[:2]))
+        as_str.append('Lower Right Corner: {}, {}\n'.format(*self.bounds[2:]))
+
+        return "".join(as_str)
         #print('[MAXIMUM]:          {}'.format(np.nanmax(self.img)))
         #print('[MINIMUM]:          {}'.format(np.nanmin(self.img)))
 
