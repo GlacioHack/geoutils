6c9d038d36dcb67e952b1758dc7b06da80e885a0
Andrew Tedstone <andrew.tedstone@unifr.ch>
2020-11-13 10:19:41 +0100

    remove a completely unnecessary CRS wrapper

--- GeoUtils/raster_tools.py
+++ GeoUtils/raster_tools.py
@@ -111,7 +111,7 @@ class Raster(object):
 
         # Enable shortcut to create CRS from an EPSG ID.
         if isinstance(crs, int):
-            crs = _create_crs_from_epsg(crs)
+            crs = CRS.from_epsg(crs)
 
         # If a 2-D ('single-band') array is passed in, give it a band dimension.
         if len(data.shape) < 3:
@@ -218,14 +218,3 @@ class SatelliteImage(Raster):
     pass
 
 
-def _create_crs_from_epsg(epsg):
-    """ Given an EPSG code, generate a rasterio CRS object.
-
-    :param epsg: the EPSG code for which to generate a CRS.
-    :dtype epsg: int
-    :returns: the CRS object
-    :rtype: rasterio.crs.CRS
-    """
-    if not isinstance(epsg, int):
-        raise ValueError('EPSG code must be provided as int.')
-    return CRS.from_epsg(epsg)
