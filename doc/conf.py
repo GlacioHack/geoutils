8aa19d87e7485bc948237386aa6ef144646d42b9
Andrew Tedstone <andrew.tedstone@unifr.ch>
2021-02-18 14:34:24 +0100

    add master_doc

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,8 @@ author = 'GeoUtils Developers'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '3.3.1'
+#needs_sphinx = '3.3.1'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
