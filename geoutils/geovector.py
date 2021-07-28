05800bca0520c587799b717dfd8056a74a6f60f9
adehecq <3285905+adehecq@users.noreply.github.com>
2021-07-28 17:33:34 +0200

    Adding method to Vector to create buffers without overlap around all geometries.

--- geoutils/geovector.py
+++ geoutils/geovector.py
@@ -16,6 +16,7 @@ from rasterio.crs import CRS
 import shapely
 from shapely.geometry.polygon import Polygon
 from scipy.spatial import Voronoi
+import matplotlib.pyplot as plt
 
 import geoutils as gu
 
@@ -323,6 +324,88 @@ the provided raster file.
         new_vector = self.__new__(type(self))
         new_vector.__init__(self.ds.query(expression))
         return new_vector  # type: ignore
+
+    def buffer_without_overlap(self, buffer_size: int | float, plot: bool = False) -> np.ndarray:
+        """
+        Returns a Vector object containing self's geometries extended by a buffer, without overlapping each other.
+
+        The algorithm is based upon this tutorial: https://statnmap.com/2020-07-31-buffer-area-for-nearest-neighbour/.
+        The buffered polygons are created using Voronoi polygons in order to delineate the "area of influence" \
+of each geometry.
+        The buffer is slightly inaccurate where two geometries touch, due to the nature of the Voronoi polygons,\
+hence one geometry "steps" slightly on the neighbor buffer in some cases.
+
+        :examples:
+        >>> outlines = gu.Vector(gu.datasets.get_path('glacier_outlines'))
+        >>> outlines = gu.Vector(outlines.ds.to_crs('EPSG:32645'))
+        >>> buffer = outlines.buffer_without_overlap(500)
+        >>> ax = buffer.ds.plot()
+        >>> outlines.ds.plot(ax=ax, ec='k', fc='none')
+        >>> plt.show()
+
+        :param buffer_size: Buffer size in self's coordinate system units.
+        :param plot: Set to True to show intermediate plots, useful for understanding or debugging.
+
+        :returns: A Vector containing the buffered geometries.
+        """
+        # Dissolve all geometries into one
+        gdf = self.ds
+        merged = gdf.dissolve()
+
+        # Add buffer around geometries
+        merged_buffer = merged.buffer(buffer_size)
+
+        # Extract only the buffered area
+        buffer = merged_buffer.difference(merged)
+
+        # Crop Voronoi polygons to bound geometry and add missing polygons
+        bound_poly = gu.projtools.bounds2poly(gdf)
+        bound_poly = bound_poly.buffer(buffer_size)
+        voronoi_all = generate_voronoi_with_bounds(gdf, bound_poly)
+        if plot:
+            plt.figure(figsize=(16, 4))
+            ax1 = plt.subplot(141)
+            voronoi_all.plot(ax=ax1)
+            gdf.plot(fc='none', ec='k', ax=ax1)
+            ax1.set_title("Voronoi polygons, cropped")
+
+        # Extract Voronoi polygons only within the buffer area
+        voronoi_diff = voronoi_all.intersection(buffer.geometry[0])
+
+        # Join attributes of original geometries into the Voronoi polygons
+        voronoi_gdf = gpd.GeoDataFrame(geometry=voronoi_diff)
+        joined_voronoi = gpd.tools.sjoin(gdf, voronoi_gdf, how='right')
+
+        # Plot results -> some polygons are duplicated
+        if plot:
+            ax2 = plt.subplot(142, sharex=ax1, sharey=ax1)
+            joined_voronoi.plot(ax=ax2, column="index_left", alpha=0.5, ec='k')
+            gdf.plot(ax=ax2, column=gdf.index.values)
+            ax2.set_title("Buffer with duplicated polygons")
+
+        # Find non unique Voronoi polygons, and retain only first one
+        _, indexes = np.unique(joined_voronoi.index, return_index=True)
+        unique_voronoi = joined_voronoi.iloc[indexes]
+
+        # Plot results -> unique polygons only
+        if plot:
+            ax3 = plt.subplot(143, sharex=ax1, sharey=ax1)
+            unique_voronoi.plot(ax=ax3, column="index_left", alpha=0.5, ec='k')
+            gdf.plot(ax=ax3, column=gdf.index.values)
+            ax3.set_title("Buffer with unique polygons")
+
+        # Dissolve all polygons by original index
+        merged_voronoi = unique_voronoi.dissolve(by='index_left')
+
+        # Plot
+        if plot:
+            ax4 = plt.subplot(144, sharex=ax1, sharey=ax1)
+            gdf.plot(ax=ax4, column=gdf.index.values)
+            merged_voronoi.plot(column=merged_voronoi.index.values, ax=ax4, alpha=0.5)
+            ax4.set_title("Final buffer")
+            plt.show()
+
+        return gu.Vector(merged_voronoi)
 
 
 ############################################
