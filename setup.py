f2c964776bd745b5a5dc6adba6578bd42f6fa47c
Andrew Tedstone <andrew.tedstone@unifr.ch>
2021-09-08 11:39:56 +0200

    Add PyPI publishing workflow (#192)
    * Add PyPI publishing workflow
    
    * Update python-publish.yml
    
    * update readme to include PyPI badge and bash example
    
    * update setup.py with additional info used by PyPI
    
    * pre-commit re-formatting
    
    * hopefully fix end of file

--- setup.py
+++ setup.py
@@ -37,18 +37,34 @@ for item in glob("geoutils/datasets/*"):
     if not bname.startswith("__"):
         data_files.append(path.join("datasets", bname))
 
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="geoutils",
     version=FULLVERSION,
     description="Tools for working with geospatial data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://www.github.com/GlacioHack/geoutils/",
     author="The GlacioHack Team",
     license="BSD-3",
     packages=["geoutils", "geoutils.datasets"],
     package_data={"geoutils": data_files},
-    install_requires=["rasterio", "geopandas", "pyproj", "scipy", "typing-extensions; python_version < '3.8'"],
+    install_requires=[
+        "rasterio",
+        "geopandas",
+        "pyproj",
+        "scipy",
+        "typing-extensions; python_version < '3.8'",
+        "matplotlib",
+    ],
     extras_require={"rioxarray": ["rioxarray"]},
     scripts=["geoutils/geoviewer.py"],
     zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+    ],
 )
