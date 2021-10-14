f5802011f9692991a8e1a3780826042c5a50e234
Andrew Tedstone <andrew.tedstone@unifr.ch>
2021-10-14 15:33:08 +0200

    fix import order

--- geoutils/georaster/__init__.py
+++ geoutils/georaster/__init__.py
@@ -1,4 +1,4 @@
+from geoutils.georaster.raster import Raster, RasterType  # noqa isort:skip
 from geoutils.georaster import spatial_tools  # noqa
-from geoutils.georaster.raster import Raster, RasterType  # noqa
 
 __all__ = ["RasterType", "Raster"]
