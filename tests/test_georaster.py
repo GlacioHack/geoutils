73e59ad9e5f5de37d9356a2c7f92f1f020c1df9d
adehecq <3285905+adehecq@users.noreply.github.com>
2022-08-12 15:17:36 +0200

    Linting

--- tests/test_georaster.py
+++ tests/test_georaster.py
@@ -499,7 +499,8 @@ class TestRaster:
         assert r.nodata is None
         assert r3.nodata == 255
 
-        # Particularly crucial if nodata falls outside the original image range -> check range is preserved (with nearest interpolation)
+        # Particularly crucial if nodata falls outside the original image range
+        # -> check range is preserved (with nearest interpolation)
         r_float = r.astype("float32")  # type: ignore
         assert r_float.nodata is None
         r3 = r_float.reproject(dst_bounds=dst_bounds, resampling="nearest")
