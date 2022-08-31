4467f9751e553e154db866a1d17ce862d5a8a406
adehecq <3285905+adehecq@users.noreply.github.com>
2022-08-31 16:25:29 +0200

    Fix issue with test_docs.py

--- doc/source/code/raster-basics_cropping_and_reprojecting.py
+++ doc/source/code/raster-basics_cropping_and_reprojecting.py
@@ -8,4 +8,4 @@ smaller_image = gu.Raster(gu.examples.get_path("everest_landsat_b4_cropped"))
 large_image_orig = large_image.copy()  # Since it gets modified inplace, we want to keep it to print stats.
 large_image.crop(smaller_image)
 # TEXT
-large_image_reprojected = large_image.reproject(smaller_image)
+large_image_reprojected = large_image_orig.reproject(smaller_image, dst_nodata=0)
