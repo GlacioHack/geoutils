9d9ff7e5bbd2ec660b36e019e179f93675ff1664
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 05:55:37 +0000

    Adding tarfile member sanitization to extractall()

--- geoutils/examples.py
+++ geoutils/examples.py
@@ -56,7 +56,26 @@ def download_examples(overwrite: bool = False) -> None:
 
         # Extract the tarball
         with tarfile.open(tar_path) as tar:
-            tar.extractall(tmp_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, tmp_dir)
 
         # Find the first directory in the temp_dir (should only be one) and construct the example data dir paths.
         for dir_name in ["Everest_Landsat", "Exploradores_ASTER"]:
