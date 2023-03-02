6ca58a0c4b0ebf5b2c9fe16ee5e6a4054c173cf0
Romain Hugonnet <romain.hugonnet@gmail.com>
2023-03-02 15:20:44 -0800

    Fix parameter description

--- geoutils/georaster/raster.py
+++ geoutils/georaster/raster.py
@@ -1710,7 +1710,7 @@ np.ndarray or number and correct dtype, the compatible nodata value.
         :param dst_size: Raster size to write to (x, y). Do not use with dst_res.
         :param dst_bounds: a BoundingBox object or a dictionary containing left, bottom, right, top bounds in the
             destination CRS.
-        :param dst_res: Pixel size in units of target CRS. Either 1 value or (xres, yres). Do not use with dst_size.
+        :param dst_res: Pixel size in units of destination CRS. Either 1 value or (xres, yres). Do not use with dst_size.
         :param dst_nodata: nodata value of the destination. If set to None, will use the same as source,
             and if source is None, will use GDAL's default.
         :param dst_dtype: Set data type of output.
