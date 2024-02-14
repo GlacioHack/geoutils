f60d388a5a4655c4f59a04cf129104af1861c2d0
Romain Hugonnet <romain.hugonnet@gmail.com>
2024-02-14 02:35:29 -0900

    Add announcement banner in doc to help users update early-dev code to `0.1` (#489)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -188,6 +188,11 @@ html_theme_options = {
         "binderhub_url": "https://mybinder.org/",
         "notebook_interface": "jupyterlab",  # For launching Binder in Jupyterlab to open MD files as notebook (downloads them otherwise)
     },
+    "announcement": (
+        "⚠️ Our 0.1 release refactored several early-development functions for long-term stability, "
+        'to update your code see <a href="https://github.com/GlacioHack/geoutils/releases/tag/v0.1.0">here</a>. ⚠️'
+        "<br>Future changes will come with deprecation warnings! 🙂"
+    )
     # "logo_only": True,
     # "icon_links": [
     #         {
