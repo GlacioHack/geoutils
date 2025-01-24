ad9967a5c3d169c0e1cad334a52a6d0bcca17493
vschaffn <valentin.schaffner@cs-soprasteria.com>
2025-01-24 16:35:43 +0100

    fix: remove rmse from  and update docstrings

--- tests/test_raster/test_raster.py
+++ tests/test_raster/test_raster.py
@@ -1962,7 +1962,6 @@ class TestRaster:
             "Sum of squares",
             "90th percentile",
             "NMAD",
-            "RMSE",
             "Standard deviation",
             "Percentile valid points",
         ]
