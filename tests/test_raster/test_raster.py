071ca8e304da31a4ad376c9aee79fa771c6cd46a
vschaffn <valentin.schaffner@cs-soprasteria.com>
2025-01-24 11:05:12 +0100

    feat: add percentile valid points in

--- tests/test_raster/test_raster.py
+++ tests/test_raster/test_raster.py
@@ -1998,13 +1998,14 @@ class TestRaster:
             "NMAD",
             "RMSE",
             "Standard deviation",
+            "Percentile valid points",
         ]
         for name in expected_stats:
             assert name in stats
             assert stats.get(name) is not None
 
         # Single stat
-        stat = raster.get_stats(stats_name="Average")
+        stat = raster.get_stats(stats_name="average")
         assert isinstance(stat, np.floating)
 
         def percentile_95(data: NDArrayNum) -> np.floating[Any]:
