284bf6ce49223a9cdaad035ee1a0e0ea7b4737c7
vschaffn <valentin.schaffner@cs-soprasteria.com>
2025-03-05 01:25:49 +0100

    fix: overload icrop

--- geoutils/raster/raster.py
+++ geoutils/raster/raster.py
@@ -2491,7 +2491,7 @@ class Raster:
         bbox: list[int] | tuple[int, ...],
         *,
         inplace: Literal[True],
-    ) -> RasterType: ...
+    ) -> None: ...
 
     @overload
     def icrop(
@@ -2499,7 +2499,7 @@ class Raster:
         bbox: list[int] | tuple[int, ...],
         *,
         inplace: Literal[False] = False,
-    ) -> None: ...
+    ) -> RasterType: ...
 
     def icrop(
         self: RasterType,
