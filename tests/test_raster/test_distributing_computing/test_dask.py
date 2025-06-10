aeb612b5a7125117cf0c793c1aaac3390f38e5b3
Romain Hugonnet <romain.hugonnet@gmail.com>
2025-06-10 15:19:20 -0800

    Add more margin for Dask memory test (#704)

--- tests/test_raster/test_distributing_computing/test_dask.py
+++ tests/test_raster/test_distributing_computing/test_dask.py
@@ -97,7 +97,7 @@ def _estimate_subsample_memusage(darr: da.Array, chunksizes_in_mem: tuple[int, i
     # Final estimate of memory usage of operation in MB
     max_op_memusage = fac_dask_margin * (chunk_memusage + sample_memusage + out_memusage + meta_memusage) / (2**20)
     # We add a base memory usage of ~130 MB + 10MB per 1000 chunks (loaded in background by Dask even on tiny data)
-    max_op_memusage += 130 + 10 * (num_chunks / 1000)
+    max_op_memusage += 150 + 10 * (num_chunks / 1000)
 
     return max_op_memusage
 
