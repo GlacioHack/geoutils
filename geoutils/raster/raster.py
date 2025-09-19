66466656fcd7b8d8a5ce7932e880a37dced944b1
Alice de Bardonnèche-Richard <alice.de-bardonneche-richard@csgroup.eu>
2025-09-19 10:58:21 +0200

    test against nanfunc

--- geoutils/raster/raster.py
+++ geoutils/raster/raster.py
@@ -3530,7 +3530,6 @@ class Raster:
         else:
             return gu.PointCloud.from_xyz(x=points[0], y=points[1], z=z, crs=self.crs)
 
-
     @overload
     def filter(
         self: RasterType,
