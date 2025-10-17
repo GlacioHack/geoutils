70d4926163c8c31f96052284a686c5d23d75166e
Alice de Bardonnèche-Richard <alice.de-bardonneche-richard@cs-soprasteria.com>
2025-10-17 14:54:19 +0200

    vectorized_adapt

--- geoutils/filters.py
+++ geoutils/filters.py
@@ -72,7 +72,8 @@ def _nan_filter(array: NDArrayNum, func: Callable[..., NDArrayNum], size: int) -
         Process NaNs values for filtering
         :param data: array containing all data
         """
-        valid = data[~np.isnan(data)]
+        flat_data = np.ravel(data)
+        valid = data[~np.isnan(flat_data)]
         return np.nan if valid.size == 0 else func(valid)
 
     return generic_filter_scipy(array, tests_on_nans, size=size, mode="constant", cval=np.nan)
