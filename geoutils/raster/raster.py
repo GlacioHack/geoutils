b1b893f4e658f14b2008af38f8f8a4d864ed3b14
marine.bouchet <marine.bouchet@csgroup.eu>
2025-12-15 10:50:53 +0100

    test read the doc list

--- geoutils/raster/raster.py
+++ geoutils/raster/raster.py
@@ -2013,16 +2013,16 @@ class Raster:
         - Sum of squares: sum of the squares of all data, ignoring masked values.
         - 90th percentile: point below which 90% of the data falls, ignoring masked values.
         - IQR (Interquartile Range): difference between the 75th and 25th percentile of a dataset, ignoring masked
-        values.
+          values.
         - LE90 (Linear Error with 90% confidence): difference between the 95th and 5th percentiles of a dataset,
-        representing the range within which 90% of the data points lie. Ignore masked values.
+          representing the range within which 90% of the data points lie. Ignore masked values.
         - NMAD (Normalized Median Absolute Deviation): robust measure of variability in the data, less sensitive to
-        outliers compared to standard deviation. Ignore masked values.
+          outliers compared to standard deviation. Ignore masked values.
         - RMSE (Root Mean Square Error): commonly used to express the magnitude of errors or variability and can give
-        insight into the spread of the data. Only relevant when the raster represents a difference of two objects.
-        Ignore masked values.
+          insight into the spread of the data. Only relevant when the raster represents a difference of two objects.
+          Ignore masked values.
         - Std (Standard deviation): measures the spread or dispersion of the data around the mean, ignoring masked
-        values.
+          values.
         - Valid count: number of finite data points in the array. It counts the non-masked elements.
         - Total count: total size of the raster.
         - Percentage valid points: ratio between Valid count and Total count.
